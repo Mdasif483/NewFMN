9f72fcb0f6ae16b551e03de0b8b6835287b8c12f
Mohd Asif <hiasif7786@gmail.com>
2025-08-25 14:21:46 +0530

    db connet

--- server/contect to db/dbconnect.js
+++ server/contect to db/dbconnect.js
@@ -10,6 +10,7 @@ const connectToDB = async () => {
     await mongoose.connect(dbURL);
     console.log("Connected to MongoDB");
   } catch (error) {
+    console.log(dbURL);
     console.error("Error connecting to MongoDB:", error);
   }
 };
